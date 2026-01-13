90a125c32c45c975d39a29157f3e430d48ad84ec
tradebitsix <tradebitsix@users.noreply.github.com>
2026-01-12 22:50:34 -0700

    Fix FastAPI app entrypoint

---
+++ frontend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.router import router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://192.0.0.4:5173",
+        "https://theo-one-market-production.up.railway.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router, prefix="/api")
